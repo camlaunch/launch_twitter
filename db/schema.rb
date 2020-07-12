2cfc094fb841adeb6bb269819e6e9b0edfeb39c1
Your Actual Name <Your Actual Emailcandrewmeyer04@gmail.com>
2020-07-12 12:21:20 -0700

    user profile association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_170313) do
+ActiveRecord::Schema.define(version: 2020_07_12_190448) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_07_12_170313) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -44,5 +54,6 @@ ActiveRecord::Schema.define(version: 2020_07_12_170313) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
   add_foreign_key "tweets", "users"
 end
